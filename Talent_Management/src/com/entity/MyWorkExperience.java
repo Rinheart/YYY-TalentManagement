74e5ab50734479a7cd2cd1904d6c01a174d1d55c
Zoe <1127210764@qq.com>
2021-01-24 16:53:30 +0800

    添加关联映射

---
+++ Talent_Management/src/com/entity/MyWorkExperience.java
@@ -0,0 +1,69 @@
+package com.entity;
+
+import java.util.Date;
+
+public class MyWorkExperience {
+    private int workExperienceId;
+    private String talentId;
+    private String enterpriseId;
+    private int departmentId;
+    private Date startTime;
+    private Date endTime;
+    private Talent talent;
+
+    public int getWorkExperienceId() {
+        return workExperienceId;
+    }
+
+    public void setWorkExperienceId(int workExperienceId) {
+        this.workExperienceId = workExperienceId;
+    }
+
+    public String getTalentId() {
+        return talentId;
+    }
+
+    public void setTalentId(String talentId) {
+        this.talentId = talentId;
+    }
+
+    public Talent getTalent() {
+        return talent;
+    }
+
+    public void setTalent(Talent talent) {
+        this.talent = talent;
+    }
+
+    public String getEnterpriseId() {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(String enterpriseId) {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}
